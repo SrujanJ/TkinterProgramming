9be354b7c98e7a5cfb47cf1984bd527d9dbab0e9
Srujan JAdhav <75870175+SrujanJ@users.noreply.github.com>
2021-03-06 14:33:44 +0530

    Create #2-Button.py

---
+++ #2-Button.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+ttk.Button(root, text="Hello World").grid()
+ttk.Button(root, text="Subscribe").grid()
+ttk.Button(root, text="Subscribe").grid()
+ttk.Button(root, text="Like And Share").grid()
+
+
+mainloop()
